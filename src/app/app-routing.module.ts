d3e8f3d6aee1f37b3fc728765c28b46e0997796f
Obtez <contact.davidbeer@gmail.com>
2022-06-06 14:39:23 +0200

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import {TermsComponent} from "./policies/terms/terms.component";
+import {DataPolicyComponent} from "./policies/data-policy/data-policy.component";
+import {CookiesPolicyComponent} from "./policies/cookies-policy/cookies-policy.component";
+import {LoginComponent} from "./login/login.component";
+import {TrialOfferComponent} from "./trial-offer/trial-offer.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // NAVBAR ROUTES
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  // TRIAL
+  { path: 'trial', component: TrialOfferComponent },
+  // POLICIES
+  { path: 'terms', component: TermsComponent },
+  { path: 'data-policy', component: DataPolicyComponent },
+  { path: 'cookies-policy', component: CookiesPolicyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeHeroComponent } from './home/home-hero/home-hero.component';
+import { SignupFormComponent } from './home/signup-form/signup-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import { TermsComponent } from './policies/terms/terms.component';
+import { DataPolicyComponent } from './policies/data-policy/data-policy.component';
+import { CookiesPolicyComponent } from './policies/cookies-policy/cookies-policy.component';
+import { LoginComponent } from './login/login.component';
+import { SocialButtonsComponent } from './social-buttons/social-buttons.component';
+import { SpacerComponent } from './spacer/spacer.component';
+import { TrialOfferComponent } from './trial-offer/trial-offer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    HomeComponent,
+    DashboardComponent,
+    HomeHeroComponent,
+    SignupFormComponent,
+    TermsComponent,
+    DataPolicyComponent,
+    CookiesPolicyComponent,
+    LoginComponent,
+    SocialButtonsComponent,
+    SpacerComponent,
+    TrialOfferComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatIconModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
